f0e53c6806b5954621dee8ecf08eb66e7a51bf88
Siming <yangsiming1999@gmail.com>
2021-05-04 23:14:47 -0700

    refactored for composite structure

---
+++ app/src/main/java/com/example/galleryapp7082/Image.java
@@ -0,0 +1,38 @@
+package com.example.galleryapp7082;
+
+import android.content.SharedPreferences;
+
+import java.io.File;
+
+public class Image implements ImageInterface{
+    private SharedPreferences.Editor editor;
+    private File file;
+    private String caption;
+    private String date;
+
+    Image(File file, String caption, String date, SharedPreferences.Editor sd){
+        this.editor = sd;
+        this.file = file;
+        this.caption = caption;
+        this.date = date;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+    public void setCaption(String newCaption) {
+        this.caption = newCaption;
+        editor.putString(this.file.getName(), newCaption);
+        editor.apply();
+    }
+
+    public String getCaption(){
+        return this.caption;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+}
